96eaf2b5f20cb1ca773f0ad8de47ea07fae0996f
Matheus Couto <couto.matheus@outlook.com>
2018-11-11 17:21:11 -0200

    feat: add fab and icon fonts

--- App.tsx
+++ App.tsx
@@ -4,7 +4,8 @@ import { StyleSheet, Platform, Image, Text, View, ScrollView } from 'react-nativ
 import firebase from 'react-native-firebase';
 
 import { Provider } from 'react-redux';
-
+import FAB from 'react-native-fab';
+import Icon from 'react-native-vector-icons/FontAwesome';
 import store from './src/store';
 
 export default class App extends React.Component {
@@ -21,46 +22,9 @@ export default class App extends React.Component {
   render() {
     return (
       <Provider store={store}>
-        <ScrollView>
           <View style={styles.container}>
-            <Image source={require('./assets/ReactNativeFirebase.png')} style={[styles.logo]}/>
-            <Text style={styles.welcome}>
-              Welcome to {'\n'} React Native Firebase
-            </Text>
-            <Text style={styles.instructions}>
-              To get started, edit App.js
-            </Text>
-            {Platform.OS === 'ios' ? (
-              <Text style={styles.instructions}>
-                Press Cmd+R to reload,{'\n'}
-                Cmd+D or shake for dev menu
-              </Text>
-            ) : (
-              <Text style={styles.instructions}>
-                Double tap R on your keyboard to reload,{'\n'}
-                Cmd+M or shake for dev menu
-              </Text>
-            )}
-            <View style={styles.modules}>
-              <Text style={styles.modulesHeader}>The following Firebase modules are pre-installed:</Text>
-              {firebase.admob.nativeModuleExists && <Text style={styles.module}>admob()</Text>}
-              {firebase.analytics.nativeModuleExists && <Text style={styles.module}>analytics()</Text>}
-              {firebase.auth.nativeModuleExists && <Text style={styles.module}>auth()</Text>}
-              {firebase.config.nativeModuleExists && <Text style={styles.module}>config()</Text>}
-              {firebase.crashlytics.nativeModuleExists && <Text style={styles.module}>crashlytics()</Text>}
-              {firebase.database.nativeModuleExists && <Text style={styles.module}>database()</Text>}
-              {firebase.firestore.nativeModuleExists && <Text style={styles.module}>firestore()</Text>}
-              {firebase.functions.nativeModuleExists && <Text style={styles.module}>functions()</Text>}
-              {firebase.iid.nativeModuleExists && <Text style={styles.module}>iid()</Text>}
-              {firebase.invites.nativeModuleExists && <Text style={styles.module}>invites()</Text>}
-              {firebase.links.nativeModuleExists && <Text style={styles.module}>links()</Text>}
-              {firebase.messaging.nativeModuleExists && <Text style={styles.module}>messaging()</Text>}
-              {firebase.notifications.nativeModuleExists && <Text style={styles.module}>notifications()</Text>}
-              {firebase.perf.nativeModuleExists && <Text style={styles.module}>perf()</Text>}
-              {firebase.storage.nativeModuleExists && <Text style={styles.module}>storage()</Text>}
-            </View>
+            <FAB buttonColor="red" iconTextColor="#FFFFFF" onClickAction={() => {console.log("FAB pressed")}} visible={true} iconTextComponent={<Icon name="plus"/>} />
           </View>
-        </ScrollView>
       </Provider> 
     );
   }
