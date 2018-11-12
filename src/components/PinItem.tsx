0f8bfbfb74419d85833e6ad72cf378111311375a
Matheus Ricardo Mendonça Couto <matheuscouto@MacBook-Pro-de-Matheus.local>
2018-11-12 20:34:16 -0200

    feat: PinItem placeholder

--- src/components/PinItem.tsx
+++ src/components/PinItem.tsx
@@ -4,7 +4,7 @@ import { View, Text, Image, TouchableOpacity, Linking } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import LinkPreview from 'react-native-link-preview';
 import { times } from 'lodash';
-
+import Placeholder from 'rn-placeholder';
 
 interface IMetaLinkData {
   images?: string[],
@@ -30,10 +30,13 @@ class PinItem extends React.Component<{ link: string, rate: number }, IMetaLinkD
   
   public render() {
     if (this.state.error) return null
-    if (!this.state.didLoad) return <View style={{ flexDirection: 'row', width: '100%', borderBottomColor: '#E1E7ED', borderBottomWidth: 1, borderStyle: 'solid' }}>
-      <View style={{ width: 100, height: 100, backgroundColor: '#E1E7ED' }} />
-      <View style={{ padding: 10, flex: 1 }} />
-    </View>
+    if (!this.state.didLoad) return <View style={{borderBottomColor: 'white', borderBottomWidth: 1, borderStyle: 'solid'}}><Placeholder.ImageContent
+                                      size={100}
+                                      animate="fade"
+                                      lineNumber={4}
+                                      lineSpacing={5}
+                                      lastLineWidth="30%"
+                                      firstLineWidth="0%" /></View>
     return (
       <TouchableOpacity style={{ flexDirection: 'row', width: '100%', borderBottomColor: '#E1E7ED', borderBottomWidth: 1, borderStyle: 'solid' }} onPress={() => Linking.openURL(this.props.link)}>
         <Image style={{ width: 100, height: 100 }} source={{ uri: this.state.images![0] }} />
