4834ca17b892fc4c6ec0b38ca81163f6d1d13660
Matheus Ricardo Mendonça Couto <matheuscouto@MacBook-Pro-de-Matheus.local>
2018-11-12 23:50:55 -0200

    refactor

--- src/screens/NewPinModal/index.tsx
+++ src/screens/NewPinModal/index.tsx
@@ -34,8 +34,6 @@ class HomeScreen extends React.Component<IMapDispatchToProps & NavigationScreenP
     return (
       <FadeInView style={{ flex: 1, alignItems: 'center', justifyContent: 'center', width: '100%', padding: 20 }} navigation={this.props.navigation}>
         <View style={{ padding: 18, width: '100%',  backgroundColor: 'white', shadowColor: 'black', shadowRadius: 5, shadowOffset: { width: 5, height: 5 }, justifyContent:"center", borderRadius: 4, alignItems: 'center'}}>
-          {/* <Text>Cole o link aqui</Text>
-          <TextInput style={{height: 40, borderRadius: 2, marginTop: 10, marginBottom: 10, width: '100%', borderColor: 'black', borderWidth: 1, borderStyle: 'solid'}} value={this.state.link} onChangeText={this.handleInputChange('link')} /> */}
           {
             this.state.isFetchingClipboard
             ? <ActivityIndicator size="small" color="black" />
@@ -63,7 +61,7 @@ class HomeScreen extends React.Component<IMapDispatchToProps & NavigationScreenP
                   ? null
                   : <View style={{width: '100%', alignItems: 'center'}}>
                       <Text style={{lineHeight: 23, fontSize: 20, textAlign: 'center', marginBottom: 10}}>Nao foi encontrado nenhum link valido :(</Text>
-                      <Text style={{lineHeight: 20}}>Copie um e volte aqui ;)</Text>
+                      <Text style={{lineHeight: 20}}>Copie um e volte aqui!</Text>
                       <Button title="Fechar" color="rgba(231,76,60,1)" onPress={() => this.props.navigation.goBack()} />
                     </View>
                 }
@@ -81,7 +79,6 @@ class HomeScreen extends React.Component<IMapDispatchToProps & NavigationScreenP
       link,
       isFetchingClipboard: false,
     }))
-    console.log('LINK: ', link)
   }
 
   handleCheckLinkValidity = (isValidLink: boolean) => {
@@ -135,8 +132,7 @@ class FadeInView extends React.Component<{style: ViewStyle} & NavigationScreenPr
       >
 
       <TouchableWithoutFeedback onPress={this.willLeaveScreen} ><View style={{flex:1, width: '100%'}} /></TouchableWithoutFeedback>
-      {this.props.children}
-
+        {this.props.children}
       <TouchableWithoutFeedback onPress={this.willLeaveScreen} ><View style={{flex:1, width: '100%'}} /></TouchableWithoutFeedback>
       </Animated.View>
     );
